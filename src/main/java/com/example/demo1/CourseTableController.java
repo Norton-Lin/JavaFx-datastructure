67c7fe3a38a37c6f7b35548672cd75213d8f51e8
Hesaier <3558131661@qq.com>
2022-06-10 10:41:36 +0800

    Revert "Merge branch 'main' of https://github.com/Norton-Lin/JavaFx-datastructure"
    This reverts commit cdaed1890c866a3858c645f60eed411da38e023f, reversing
    changes made to 8968220fb004641e7b01ecfa881a34be9759e002.

--- src/main/java/com/example/demo1/CourseTableController.java
+++ src/main/java/com/example/demo1/CourseTableController.java
@@ -7,14 +7,11 @@ import com.example.demo1.Code.systemtime.SystemTime;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class CourseTableController {
     //控制当前Controller的场景
@@ -58,11 +55,13 @@ public class CourseTableController {
         //实例化查找对象
         search = new Search();
 
+        BuildTheTable();
+
         try {
             //加载FXML文件
             FXMLLoader loader = new FXMLLoader(getClass().getResource("CourseTable.fxml"));
             loader.setController(this);
-            thisStage.setScene(new Scene(loader.load(), 1800, 700));
+            thisStage.setScene(new Scene(loader.load(), 900, 200));
             thisStage.setTitle("欢迎来到课程表~");
         } catch (IOException e) {
             e.printStackTrace();
@@ -71,7 +70,6 @@ public class CourseTableController {
 
     @FXML
     private void initialize() {
-        BuildTheTable();
         //点击回到主菜单后的反应
         this.BackToMain.setOnAction(event -> BackToMainMenu());
         //点击查找按钮后的反应
@@ -126,21 +124,15 @@ public class CourseTableController {
                     + temp.getM_tTime().getEndHour() + ":"
                     + temp.getM_tTime().getEndMinute()
                     + "\n" + "上课地点：" + temp.getM_sConstruction()
-                    + temp.getM_iFloor() + "层" + temp.getM_iRoom() + "室" + "\n";
-            if (temp.getM_cExamTime().getStartHour() != -1) {
-                Info = Info +"考试时间：" + temp.getM_cExamTime().getStartMonth()
-                        + "月" + temp.getM_cExamTime().getStartDate() + "日"
-                        + "星期" + temp.getM_cExamTime().getWeek() + "\t"
-                        + temp.getM_cExamTime().getStartHour() + ":"
-                        + temp.getM_cExamTime().getStartMinute() + "~"
-                        + temp.getM_cExamTime().getEndHour() + ":"
-                        + temp.getM_cExamTime().getEndMinute() + "\n";
-            } else
-                Info = Info + "考试时间未公布！\n";
-            if (!temp.getM_cExamConstruction().toString().equals(""))
-                Info = Info + "考试地点：" + temp.getM_cExamConstruction();
-            else
-                Info = Info + "考试地点未公布！\n";
+                    + temp.getM_iFloor() + "层" + temp.getM_iRoom() + "室"
+                    + "\n" + "考试时间：" + temp.getM_cExamTime().getStartMonth()
+                    + "月" + temp.getM_cExamTime().getStartDate() + "日"
+                    + "星期" + temp.getM_cExamTime().getWeek() + "\t"
+                    + temp.getM_cExamTime().getStartHour() + ":"
+                    + temp.getM_cExamTime().getStartMinute() + "~"
+                    + temp.getM_cExamTime().getEndHour() + ":"
+                    + temp.getM_cExamTime().getEndMinute()
+                    + "\n" + "考试地点：" + temp.getM_cExamConstruction();
         } else {
             Info = "课表中无此课程，查找失败！";
         }
@@ -148,65 +140,10 @@ public class CourseTableController {
     }
 
     protected void BuildTheTable() {
-        if (this.courses != null) {
-            //存储一周的课表
-            ArrayList<String> Mon = new ArrayList<>();
-            ArrayList<String> Tue = new ArrayList<>();
-            ArrayList<String> Wed = new ArrayList<>();
-            ArrayList<String> Thu = new ArrayList<>();
-            ArrayList<String> Fri = new ArrayList<>();
-            ArrayList<String> Sat = new ArrayList<>();
-            ArrayList<String> Sun = new ArrayList<>();
-            StringBuilder course_time = new StringBuilder();
-            int[] values;
-            for (Course course : this.courses) {
-                course_time.delete(0, course_time.length());
-                course_time.append(course.getM_tTime().getStartHour()).append(course.getM_tTime().getStartMinute())
-                        .append(course.getM_tTime().getEndHour()).append(course.getM_tTime().getEndMinute());
-                values = getSex(course_time.toString());
-                switch (course.getM_tTime().getWeek()) {
-                    case 1 -> Mon.add(course + "\b上课时间为" + Arrays.toString(values) + "节\t");
-                    case 2 -> Tue.add(course + "\b上课时间为" + Arrays.toString(values) + "节\t");
-                    case 3 -> Wed.add(course + "\b上课时间为" + Arrays.toString(values) + "节\t");
-                    case 4 -> Thu.add(course + "\b上课时间为" + Arrays.toString(values) + "节\t");
-                    case 5 -> Fri.add(course + "\b上课时间为" + Arrays.toString(values) + "节\t");
-                    case 6 -> Sat.add(course + "\b上课时间为" + Arrays.toString(values) + "节\t");
-                    case 7 -> Sun.add(course + "\b上课时间为" + Arrays.toString(values) + "节\t");
-                }
-            }
-            StringBuilder result = new StringBuilder();
-            if (!Mon.isEmpty())
-                result.append("星期一：").append(Mon).append("\n\n");
-            else
-                result.append("星期一无课程\n\n");
-            if (!Tue.isEmpty())
-                result.append("星期二：").append(Tue).append("\n\n");
-            else
-                result.append("星期二无课程\n\n");
-            if (!Wed.isEmpty())
-                result.append("星期三：").append(Wed).append("\n\n");
-            else
-                result.append("星期三无课程\n\n");
-            if (!Thu.isEmpty())
-                result.append("星期四：").append(Thu).append("\n\n");
-            else
-                result.append("星期四无课程\n\n");
-            if (!Fri.isEmpty())
-                result.append("星期五：").append(Fri).append("\n\n");
-            else
-                result.append("星期五无课程\n\n");
-            if (!Sat.isEmpty())
-                result.append("星期六：").append(Sat).append("\n\n");
-            else
-                result.append("星期六无课程\n\n");
-            if (!Sun.isEmpty())
-                result.append("星期日：").append(Sun).append("\n\n");
-            else
-                result.append("星期日无课程\n\n");
+        //存储一周的课表
+        String[][] AWeek = new String[7][14];
+        for (Course course : this.courses) {
 
-            this.CourseTable.setText(result.toString());
-        } else {
-            this.CourseTable.setText("无课程！");
         }
     }
 }
